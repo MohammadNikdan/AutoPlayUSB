2c733aae0d3d3be631441e35eefcfc32f8020aaf
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2025-09-04 16:29:33 +0330

    Update Color.kt

--- app/src/main/java/com/example/autoplayusb/ui/theme/Color.kt
+++ app/src/main/java/com/example/autoplayusb/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.example.autoplayusb.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val md_theme_light_primary = Color(0xFF006C4E)
+val md_theme_light_primary = Color(0xFF6750A4)
 val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFF88F7C3)
-val md_theme_light_onPrimaryContainer = Color(0xFF002112)
+val md_theme_light_primaryContainer = Color(0xFFEADDFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF21005D)
+
+val md_theme_dark_primary = Color(0xFFD0BCFF)
+val md_theme_dark_onPrimary = Color(0xFF381E72)
+val md_theme_dark_primaryContainer = Color(0xFF4F378B)
+val md_theme_dark_onPrimaryContainer = Color(0xFFEADDFF)
