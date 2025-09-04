03eb09f4c222d5fd22612291b820fe1e2274bed1
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2025-09-04 16:30:23 +0330

    Update Theme.kt

--- app/src/main/java/com/example/autoplayusb/ui/theme/Theme.kt
+++ app/src/main/java/com/example/autoplayusb/ui/theme/Theme.kt
@@ -1,18 +1,23 @@
 package com.example.autoplayusb.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
+private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary
+private val DarkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer
 )
 
 @Composable
@@ -20,11 +25,11 @@ fun AutoPlayUSBTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
+    val colors = if (darkTheme) DarkColors else LightColors
 
     MaterialTheme(
         colorScheme = colors,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
