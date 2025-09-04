50d662746f3ea982e49bb9ef03a57de1f20d94e1
MohammadNikdan <110032445+MohammadNikdan@users.noreply.github.com>
2025-09-04 16:22:20 +0330

    Update Theme.kt

--- app/src/main/java/com/example/autoplayusb/ui/theme/Theme.kt
+++ app/src/main/java/com/example/autoplayusb/ui/theme/Theme.kt
@@ -1,41 +1,29 @@
 package com.example.autoplayusb.ui.theme
 
-import android.os.Build
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColors = darkColorScheme(
+private val DarkColorScheme = darkColorScheme(
     primary = md_theme_light_primary,
-    secondary = md_theme_light_secondary
+    onPrimary = md_theme_light_onPrimary
 )
 
-private val LightColors = lightColorScheme(
+private val LightColorScheme = lightColorScheme(
     primary = md_theme_light_primary,
-    secondary = md_theme_light_secondary
+    onPrimary = md_theme_light_onPrimary
 )
 
 @Composable
 fun AutoPlayUSBTheme(
-    darkTheme: Boolean = false,
-    dynamicColor: Boolean = true,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColors
-        else -> LightColors
-    }
+    val colors = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
